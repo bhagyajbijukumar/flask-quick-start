5ec6cc4809fb1ef85122d01bc7fde63d3c20a70e
Bhagyajkumar Bijukumar <bhagyajbijukumar@gmail.com>
2020-12-05 18:50:45 +0530

    fixed migration

--- migrate.py
+++ migrate.py
@@ -1,7 +1,7 @@
 from main import create_app
+from extensions import db
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
-from models import *
 
 
 app = create_app(config_file="config.cfg")
