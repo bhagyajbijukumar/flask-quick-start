d9a229c2829c722b2cd4bc9344884e329fdcf5c0
Bhagyajkumar Bijukumar <bhagyajbijukumar@gmail.com>
2020-12-05 15:48:17 +0530

    starting migration

---
+++ migrate.py
@@ -0,0 +1,13 @@
+from main import create_app
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
+from models import *
+
+
+app = create_app(config_file="config.cfg")
+
+manager = Manager(app)
+manager.add_command("db", MigrateCommand)
+
+if __name__ == "__main__":
+    manager.run()
